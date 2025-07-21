90d403b71de3b03ed0060a5dec9ccc0cd32e318c
Rhenan Leite <rhenan@email.com>
2025-07-20 23:54:31 -0300

    Adding sprites, background and songs Creating menu with background, song and options (just visuals) Creating const file

---
+++ code/Const.py
@@ -0,0 +1,16 @@
+# C
+# Menu Theme Color, if you want to change the color, just change the numbers inside the parameters
+COLOR_CYAN = (0, 255, 255)  # Title Menu Text
+COLOR_WHITE = (255, 255, 255)
+
+# M
+
+MENU_OPTION = ('NEW GAME',
+               'TUTORIAL',
+               'SCORE',
+               'QUIT')
+
+# W
+# Screen Width and Height, change automatically to Game.py
+WIN_WIDTH = 576
+WIN_HEIGHT = 324
--- code/Menu.py
+++ code/Menu.py
@@ -1,9 +1,39 @@
 #!/usr/bin/python
 # -*- coding: utf-8 -*-
+import pygame.image
+from pygame import Surface, Rect
+from pygame.font import Font
+
+from code.Const import WIN_WIDTH, COLOR_CYAN, MENU_OPTION, COLOR_WHITE
+
 
 class Menu:
     def __init__(self, window):
         self.window = window
+        self.surf = pygame.image.load('./asset/MenuBg.png')
+        self.rect = self.surf.get_rect(left=0, top=0)
 
     def run(self, ):
-        pass
+        pygame.mixer_music.load('./asset/MenuSong.mp3')
+        pygame.mixer_music.play(-1)
+        while True:
+            self.window.blit(source=self.surf, dest=self.rect)
+            self.menu_text(50, "Sky", COLOR_CYAN, ((WIN_WIDTH / 2), 50))
+            self.menu_text(50, "Shooter", COLOR_CYAN, ((WIN_WIDTH / 2), 100))
+
+            for i in range(len(MENU_OPTION)):
+                self.menu_text(30, MENU_OPTION[i], COLOR_WHITE, ((WIN_WIDTH / 2), 200 + 32 * i))
+
+            pygame.display.flip()
+
+            # Check for all events
+            for event in pygame.event.get():
+                if event.type == pygame.QUIT:
+                    pygame.quit()  # Close Window
+                    quit()  # End pygame
+
+    def menu_text(self, text_size: int, text: str, text_color: tuple, text_center_pos: tuple):
+        text_font: Font = pygame.font.SysFont(name="Times New Roman", size=text_size)
+        text_surf: Surface = text_font.render(text, True, text_color).convert_alpha()
+        text_rect: Rect = text_surf.get_rect(center=text_center_pos)
+        self.window.blit(source=text_surf, dest=text_rect)
